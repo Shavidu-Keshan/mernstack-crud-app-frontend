f8105a284b612c39d5688679a4b22a8072bcfbd7
Shavidu Keshan <shavidu4321@gmail.com>
2024-08-15 16:24:35 +0530

    conecct the url for display database data

--- src/Components/UserDetails/Users.js
+++ src/Components/UserDetails/Users.js
@@ -1,11 +1,18 @@
 import React from "react";
 import Nav from "../Nav/Nav";
+import axios from "axios";
+const url="http://Localhost:5000/users";
+
+const fetchHandler = async () => {
+  return await axios.get(URL).then((res) => res.data);
+}
 
 function Users() {
+  
   return (
     <div>
       <Nav />
-      <h1>userdeatails</h1>
+      <h1>User Details Display Page</h1>
     </div>
   );
 }
