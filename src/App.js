282d3ed61f2486b615fd7a5432af879a2f08c864
Shavidu Keshan <shavidu4321@gmail.com>
2024-08-15 12:53:09 +0530

    link to navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import React from "react";
 import "./App.css";
 import Home from "./Components/Home/Home"; 
+import { Route, Routes } from "react-router-dom";
 //import nav from "./Components/nav/nav";
 
 function App() {
   return (
     <div>
       <Home></Home>
+      <React.Fragment>
+        <Routes>
+          <Route path="/mainhome" element={<Home/>}/>
+        </Routes>
+      </React.Fragment>
     </div>
   );
 }
--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,18 +1,25 @@
 import React from 'react'
 import './Nav.css'
+import { Link } from 'react-router-dom'
 
 function Nav() {
   return (
     <div>
       <ul className="home-ul">
         <li className='home-ll'>
+            <Link to="/mainhome"className="active home-a">
             <h1>home</h1>
+            </Link>
         </li>
         <li className='home-ll'>
+        <Link to="/adduser"className="active home-a">
             <h1>ADD user</h1>
+            </Link>
         </li>
         <li className='home-ll'>
+        <Link to="/adduser"className="active home-a">
             <h1>user details</h1>
+            </Link>
         </li>
       </ul>
     </div>
