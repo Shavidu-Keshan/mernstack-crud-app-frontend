c440168a189f8cc6c91965857ad0ebb33d698a83
Shavidu Keshan <shavidu4321@gmail.com>
2024-08-20 13:26:36 +0530

    crate form

--- src/Components/AddUser/AddUser.js
+++ src/Components/AddUser/AddUser.js
@@ -3,17 +3,38 @@ import Nav from "../Nav/Nav";
 import { useNavigate } from "react-router-dom";
 
 function AddUser() {
-    const history = useNavigate();
-    const [inputs,setInputs] = useState({
-         name:"",
-         gmail:"",
-         age:"",
-         address:"",
-    })
+  const history = useNavigate();
+  const [inputs, setInputs] = useState({
+    name: "",
+    gmail: "",
+    age: "",
+    address: "",
+  });
   return (
     <div>
       <Nav />
       <h1>Add user</h1>
+      <form>
+        <div>
+          <label>Name: </label>
+          <input type="text" name="name" value={inputs.name} required />
+        </div>
+        <div>
+          <label>Gmail: </label>
+          <input type="email" name="gmail" value={inputs.gmail} required />
+        </div>
+        <div>
+          <label>Age: </label>
+          <input type="number" name="age" value={inputs.age} required />
+        </div>
+        <div>
+          <label>Address: </label>
+          <input type="text" name="address" value={inputs.address} required />
+        </div>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
     </div>
   );
 }
