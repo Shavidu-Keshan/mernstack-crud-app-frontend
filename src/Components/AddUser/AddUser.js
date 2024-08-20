e3d069c288fb272f744ad0b81bcdc1b7f57e0dd1
Shavidu Keshan <shavidu4321@gmail.com>
2024-08-20 13:00:14 +0530

    create use navigate function and insert data call part

--- src/Components/AddUser/AddUser.js
+++ src/Components/AddUser/AddUser.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Nav from "../Nav/Nav";
+import { useNavigate } from "react-router-dom";
 
 function AddUser() {
+    const history = useNavigate();
+    const [inputs,setInputs] = useState({
+         name:"",
+         gmail:"",
+         age:"",
+         address:"",
+    })
   return (
     <div>
       <Nav />
